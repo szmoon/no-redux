49d674ad4f77c474fd94af10f86b10fb919930f2
szmoon <szmoon@gmail.com>
2019-04-22 20:25:56 -0700

    add Store.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Store } from './Store';
 
 export default function App() {
+  const store = React.useContext(Store);
+
   return (
     <React.Fragment>
+      {console.log(store)}
       <div>
         <h1>Rick and Morty</h1>
         <p>Pick your favourite episodes</p>
